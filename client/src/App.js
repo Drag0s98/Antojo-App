20b9dd94a506e0d46dc3cee3168d0178940f2ff6
Christian <hiperboreos@gmail.com>
2021-10-20 18:44:41 +0200

    registro y login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,56 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import './styles/styles.scss'
 
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import {auth} from './firebase'
+import React from 'react';
+import Reset from './components/Reset/Reset';
 
 function App() {
-  return (
-    <div className="App">
+
+  const [firebaseUser, setFirebaseUser] = React.useState(false)
+
+  React.useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if(user) {
+        setFirebaseUser(user)
+      } else {
+        setFirebaseUser(null)
+      }
+    })
+  }, [])
+
+
+  return firebaseUser !== false ? (
+    <div className="container">
       <BrowserRouter>
-        <Header />
-        <Main />
+        <Navbar firebaseUser={firebaseUser}/>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+            </Route>
+            <Route path='/login'>
+              <Login />
+              </Route>
+              <Route path='/reset'>
+              <Reset />
+              </Route>
+             <Header />
+             <Main />
+
+        </Switch>
       </BrowserRouter>
       <Footer />
     </div>
-  );
+  ) : (
+    <p>Cargando...</p>
+  )
 }
 
 export default App;
