534324552f3489f1858f186b546102366abfff98
Tamara <tamaragmartin@gmail.com>
2021-10-21 22:14:05 +0200

    onboarding screen setup

--- client/src/components/Onboarding/Onboarding.jsx
+++ client/src/components/Onboarding/Onboarding.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
+
+import image1 from "../../assets/slide1.jpg";
+import image2 from "../../assets/slide2.jpg";
 
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  const [index, setIndex] = useState(0);
+  
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <section className="onboarding">
+      <article className="onboarding--text">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim velit mollit. Exercitation
+        veniam consequat sunt nostrud amet.
+      </article>
+      <article className="onboarding--carousel">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image1} alt="First slide" />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image2} alt="Second slide" />
+            <Carousel.Caption></Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </article>
+      <Link to={`/home`}>
+        <button type="submit" name="button" className="onboarding--btn">
+          Saltar
+        </button>
+      </Link>
+    </section>
+  );
 };
 
 export default Onboarding;
