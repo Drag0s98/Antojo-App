a621b705ea6c8e1a7c3b06d441e291ebc8cefc10
Drag0s98 <dragostanase1698@gmail.com>
2021-10-21 22:53:28 +0200

    funtionality of home done

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Browser from '../Browser';
+import Map from '../Map';
+import axios_hook from '../../hooks/get-axios'
+
 
 const Home = () => {
+
+  const [ranking, setRanking] = useState(null);
+
+  const { loading, result } = axios_hook('http://localhost:5000/api/dishes');
+
+  useEffect(() => {
+    console.log(result)
+    let numA = 5;
+    let arr = []
+    result.filter((element) => {
+      if (element.rating < numA || element.rating === 5) {
+        numA = element.rating;
+        return arr.push(element);
+      } else {
+        return null;
+      }
+    })
+    setRanking(arr);
+  }, [loading, result])
+
   return (
     <section>
       <Browser />
-    <p>Esto es el home</p>
+      <Map />
+      <article className='ranking_box' >
+        <h2>Ranking </h2>
+        {ranking != null ? ranking.map((param, i) => {
+          return (
+            <p key={i}>
+              {param.name}
+            </p>
+          )
+        }) : ''}
+      </article>
+      <br />
+      <article className='offer_box'>
+        <h2>Ofertas</h2>
+        {ranking != null ? ranking.map((param, i) => {
+          if (param.offer === true) {
+            return (
+              <p key={i}>
+                {param.name}
+              </p>
+            )
+          } else {
+            return null;
+          }
+        }) : ''}
+      </article>
+      <article className='recomendation_box'>
+        <h2>Recomendacioens</h2>
+        <p>Aqui irian las recomendaciones</p>
+      </article>
     </section>
   );
 };
