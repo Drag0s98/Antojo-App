ce3e105163a631b3e599bd1dc1da9b6feecfec05
Drag0s98 <dragostanase1698@gmail.com>
2021-10-26 21:38:04 +0200

    orders done

--- client/src/components/Orders_List/Orders_List.jsx
+++ client/src/components/Orders_List/Orders_List.jsx
@@ -1,20 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
+import { DataContext } from "../../context/context";
 
 const Orders_List = () => {
+
+  const [order, setOrder] = useState(null);
+  const { uid } = useContext(DataContext);
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/orders/${uid}`)
+      .then((res) => setOrder(res.data))
+  }, [uid])
+
+  //Lo del tiempo creo que lo puedo sacar pero necesito mirarlo mas en profundidad.
+console.log(order);
   return (
-    <section>
-      <img src="" alt="" />
-      <h3>Estado del pedido</h3>
-      <p>Plato</p>
-      <p>Tiempo</p>
-      <p>Precio</p>
-      <p>En camino</p>
-      <Link to={`/orderdetails`}>
-        <button>Ver pedido</button>
-      </Link>
-    </section>
+    order != null ?
+      <section>
+        {order.map((param, i) => {
+          return (
+            <article key={i}>
+              <img src="" alt="" />
+              <h3>Estado del pedido </h3>
+              <p>Plato {param.dish_name}</p>
+              <p>Tiempo </p>
+              <p>Precio {param.price}</p>
+              <p>En camino</p>
+              <button >Ver pedido</button>
+            </article>
+          )
+        })}
+      </section>
+      : setTimeout(() => {
+        <Redirect to='/login' />
+      }, 200)
   );
 };
 
+//<Link to={`/orderdetails`}>
 export default Orders_List;
