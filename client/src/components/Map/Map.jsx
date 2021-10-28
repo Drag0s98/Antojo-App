237006a3e22e7ebac9e605fe4c2d995fa46342e9
Tamara <tamaragmartin@gmail.com>
2021-10-28 17:02:35 +0200

    styles updated

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -32,7 +32,7 @@ const Map = ({ watch, settings }) => {
             center={geolocation}
             zoom={13}
             scrollWheelZoom={false}
-            style={{ height: "60vh", width: "94vw" }}
+            style={{ height: "50vh", width: "94vw" }}
           >
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
