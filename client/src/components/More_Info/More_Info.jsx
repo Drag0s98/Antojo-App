532affe9717757b92c55766f79aa56f4e4b2338d
Tamara <tamaragmartin@gmail.com>
2021-10-28 10:28:57 +0200

    mini map added at more info

--- client/src/components/More_Info/More_Info.jsx
+++ client/src/components/More_Info/More_Info.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import { DataContext } from '../../context/context';
+import { DataContext } from "../../context/context";
 
-import Footer from '../Footer';
-import swal from 'sweetalert';
+import Footer from "../Footer";
+import swal from "sweetalert";
 
-
-import { MapContainer, TileLayer, Marker, Popup, MapConsumer } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  MapConsumer,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIconPng from "leaflet/dist/images/marker-icon.png";
 import { Icon } from "leaflet";
@@ -28,8 +33,8 @@ function More_Info({ location }) {
   const objRestaurant = {
     name: location.state.restaurant.name,
     address: location.state.restaurant.address,
-    coordinates: location.state.restaurant.coordinates
-  }
+    coordinates: location.state.restaurant.coordinates,
+  };
 
   useEffect(() => {
     axios
@@ -41,42 +46,71 @@ function More_Info({ location }) {
       });
   }, [location]);
 
-  const coordinates = [40.42166, -3.69271] // Coger coordenadas del restaurante
+  const coordinates = [40.42166, -3.69271]; // Coger coordenadas del restaurante
 
   return (
     <section className="moreInfo">
       <header className="header-general">
-        <button onClick={() => history.push('/home')}>«--</button>
+        <button onClick={() => history.push("/home")}>«--</button>
         <h3>Nombre del plato</h3>
       </header>
       <div className="tgview">
-        <h2>Plato:</h2>  <h4 className="tituloview">{location.state.dish.name}</h4>
+        <h2>Plato:</h2>{" "}
+        <h4 className="tituloview">{location.state.dish.name}</h4>
         Categoría: <p>{location.state.dish.category}</p>
         Restaurante: <h3>{location.state.restaurant}</h3>
         Precio: <p>{location.state.dish.price}</p>
-        <button onClick={() => {
-          swal({
-            title: '¡Su plato ha sido añadido a pedidos!',
-            text: '¿Deseas pagar ya el plato?',
-            buttons: true,
-            className: 'popUp'
-          })
-          .then(async(value) => {
-            if(value){
-              await new Promise(resolve => setTimeout(resolve, 200))
-              history.push('/address')
-            }else{
-              swal.close();
-            }
-          })
-          
-        }} >
+        <button
+          onClick={() => {
+            swal({
+              title: "¡Su plato ha sido añadido a pedidos!",
+              text: "¿Deseas pagar ya el plato?",
+              buttons: true,
+              className: "popUp",
+            }).then(async (value) => {
+              if (value) {
+                await new Promise((resolve) => setTimeout(resolve, 200));
+                history.push("/address");
+              } else {
+                swal.close();
+              }
+            });
+          }}
+        >
           Pedir plato
         </button>
+        <section>
+          <MapContainer
+            center={coordinates}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "30vh", width: "94vw" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={`https://api.mapbox.com/styles/v1/tamaragmartin/ckuykd13k01un14of1yunvqli/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_KEY}`}
+            />
+            <Marker
+              position={coordinates}
+              icon={
+                new Icon({
+                  iconUrl: markerIconPng,
+                  iconSize: [25, 41],
+                  iconAnchor: [12, 41],
+                })
+              }
+            >
+              <Popup>
+                Nombre restaurante <br /> dirección <br />
+              </Popup>
+            </Marker>
+            );
+          </MapContainer>
+        </section>
         <Footer />
       </div>
     </section>
   );
-}
+};
 
 export default More_Info;
